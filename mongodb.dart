ac5c028c9822122a99a493a00cf071fd673c0ccd
Jithu <129660199+JithuMorrison@users.noreply.github.com>
2024-12-11 17:19:18 +0530

    Update mongodb.dart

--- mongodb.dart
+++ mongodb.dart
@@ -1,11 +1,12 @@
 import 'package:mongo_dart/mongo_dart.dart';
 import 'package:onestop/announcementmodel.dart';
 import 'package:onestop/studentmodel.dart';
+import 'calendareventsmodel.dart';
 import 'dbhelper.dart';
 import 'mongodbmodel.dart';
 
 class MongoDatabase {
-  static var db, userCollection,announcements,attendance,cgpa;
+  static var db, userCollection,announcements,attendance,cgpa,calendarevent;
   static connect() async {
     db = await Db.create(MONGO_CONN_URL);
     await db.open();
@@ -13,6 +14,7 @@ class MongoDatabase {
     announcements = db.collection(ANNOUNCEMENTS);
     attendance = db.collection(ATTENDANCE);
     cgpa = db.collection(CGPA);
+    calendarevent = db.collection(CALENDAREVENT);
     if(db.isConnected){
       print("Successfully connected");
     }
@@ -35,6 +37,20 @@ class MongoDatabase {
   static Future<String> insertclass(StudentModel data) async {
     try {
       var result = await attendance.insertOne(data.toJson());
+      if (result.isSuccess) {
+        return "Data Inserted Successfully";
+      } else {
+        return "Data Insertion Failed";
+      }
+    } catch (e) {
+      print("Error during insertion: $e");
+      return e.toString();
+    }
+  }
+
+  static Future<String> insertCalendarEvent(CalendarEventModel data) async {
+    try {
+      var result = await calendarevent.insertOne(data.toJson());
       if (result.isSuccess) {
         return "Data Inserted Successfully";
       } else {
@@ -71,6 +87,9 @@ class MongoDatabase {
           .set('phone_number', data.phoneNumber)
           .set('role', data.role)
           .set('dob', data.dob)
+          .set('dept',data.dept)
+          .set('section',data.section)
+          .set('year',data.year)
           .set('credit', data.credit)
           .set('events', data.events.map((event) => event.toJson()).toList());
 
@@ -86,8 +105,33 @@ class MongoDatabase {
     }
   }
 
+  static Future<String> updateCalendarEvent(CalendarEventModel data) async {
+    try {
+      // Define the filter to find the document by its ObjectId
+      var filter = where.eq('_id', data.id);
+
+      // Create the update operation
+      var updateOperation = modify
+          .set('type', data.type)
+          .set('events', data.events.map((event) => event.toJson()).toList());
+
+      // Perform the update
+      var result = await calendarevent.update(filter, updateOperation);
+
+      // Check the result of the update
+      if (result['nModified'] > 0) {
+        return "Calendar Event Updated Successfully";
+      } else {
+        return "No Calendar Event Updated";
+      }
+    } catch (e) {
+      print("Error during update: $e");
+      return e.toString();
+    }
+  }
+
   /// Delete a document
-  static Future<String> delete(String id) async {
+  static Future<String> delete(ObjectId id) async {
     try {
       var result = await userCollection.remove(where.eq('_id', id));
       if (result['n'] > 0) {
