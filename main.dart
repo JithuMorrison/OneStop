a73810ad8cd48a135b84b4e0ed5ca2ad234bb41f
Jithu <129660199+JithuMorrison@users.noreply.github.com>
2024-12-11 17:18:48 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:onestop/dashboard.dart';
+import 'package:onestop/dbhelper.dart';
 import 'package:onestop/login.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'mongodb.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await MongoDatabase.connect();
+  await Supabase.initialize(
+    url: SUPABASEURL,
+    anonKey: SUPABASEKEY,
+  );
   runApp(const MyApp());
 }
 
