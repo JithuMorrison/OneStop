2c4614b4fc22ce64aded87cfd1c7d85f8a05a877
Jithu <129660199+JithuMorrison@users.noreply.github.com>
2024-12-11 17:16:10 +0530

    Update dashboard.dart

--- dashboard.dart
+++ dashboard.dart
@@ -1,10 +1,15 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:onestop/announcementpage.dart';
 import 'package:onestop/attendancepage.dart';
+import 'package:onestop/blogpage.dart';
 import 'package:onestop/calendar.dart';
 import 'package:onestop/cgpacalculator.dart';
+import 'package:onestop/donatepage.dart';
 import 'package:onestop/examschedules.dart';
 import 'package:onestop/login.dart';
+import 'package:onestop/profile.dart';
 import 'package:onestop/trackattendance.dart';
 import 'mongodbmodel.dart';
 
@@ -34,7 +39,6 @@ class _DashboardState extends State<Dashboard> {
         ),
         backgroundColor: Colors.blue,
         elevation: 3,
-        centerTitle: true,
         actions: [
           Row(
             children: [
@@ -44,12 +48,26 @@ class _DashboardState extends State<Dashboard> {
                 '${widget.user.credit}',
                 style: const TextStyle(fontSize: 18),
               ),
-              ElevatedButton(onPressed: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context)=>LoginPage()));
-              }, child: Icon(Icons.logout),
-              style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.transparent),
-              elevation: MaterialStateProperty.all(0),
-              )),
+              SizedBox(
+                width: 40,
+                child: ElevatedButton(onPressed: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=>ProfilePage(user: widget.user)));
+                }, child: Icon(Icons.supervised_user_circle_outlined),
+                    style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.transparent),
+                      elevation: MaterialStateProperty.all(0),
+                      padding: MaterialStateProperty.all(EdgeInsets.zero),
+                    )),
+              ),
+              SizedBox(
+                width: 40,
+                child: ElevatedButton(onPressed: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=>LoginPage()));
+                }, child: Icon(Icons.logout),
+                  style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.transparent),
+                  elevation: MaterialStateProperty.all(0),
+                  padding: MaterialStateProperty.all(EdgeInsets.zero),
+                )),
+              ),
             ],
           ),
         ],
@@ -86,20 +104,115 @@ class _DashboardState extends State<Dashboard> {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              Card(
-                elevation: 5,
-                margin: EdgeInsets.all(16),
-                child: Padding(
-                  padding: EdgeInsets.all(16),
-                  child: Text(
-                    "Hello, ${widget.user.name}!!",
-                    style: TextStyle(fontSize: 20),
+              Transform.translate(
+                offset: Offset(10, 0),
+                child: Row(
+                  children: [
+                    Card(
+                      elevation: 5,
+                      margin: EdgeInsets.all(16),
+                      child: Padding(
+                        padding: EdgeInsets.all(16),
+                        child: Text(
+                          "Hello, ${widget.user.name}!!",
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ),
+                    ),
+                SizedBox(
+                  width: 60,
+                  height: 60,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => CalendarPage(user: widget.user,))
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      padding: EdgeInsets.zero,
+                      elevation: 3,
+                    ),
+                    child: const Icon(
+                      Icons.calendar_month,
+                      size: 24, // Optional: adjust size if needed
+                    ),
                   ),
                 ),
+                  ],
+                ),
               ),
-              ElevatedButton(onPressed: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context)=>CalendarPage(user: widget.user,)));
-              }, child: const Text("Click")),
+              Transform.translate(
+                offset: Offset(40, 0),
+                child: Row(
+                  children: [
+                    SizedBox(
+                      width: 120,
+                      height: 60,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => BlogPage())
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          padding: EdgeInsets.zero,
+                          elevation: 3,
+                        ),
+                        child: const Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.book_outlined,
+                              size: 24, // Optional: adjust size if needed
+                            ),
+                            Text("Blog Page"),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 30,),
+                    SizedBox(
+                      width: 120,
+                      height: 60,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => DonatePage(user: widget.user))
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          padding: EdgeInsets.zero,
+                          elevation: 3,
+                        ),
+                        child: const Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.monetization_on_outlined,
+                              size: 24, // Optional: adjust size if needed
+                            ),
+                            Text("Donate"),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
               Card(
                 elevation: 3,
                 margin: EdgeInsets.all(16),
